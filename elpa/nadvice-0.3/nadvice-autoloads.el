403cea86edc5977b6d9598f59ec4d6f9516aa21f
ruleless <ruleless@126.com>
2019-07-26 04:15:54 -0400

    将elpa也提取出来，以提供离线安装

---
+++ elpa/nadvice-0.3/nadvice-autoloads.el
@@ -0,0 +1,15 @@
+;;; nadvice-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("nadvice.el") (23866 46188 411986 164000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; nadvice-autoloads.el ends here
