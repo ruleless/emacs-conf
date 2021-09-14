2ade859f13f028a28266c4ea2c9a66e741f035f8
ruleless <ruleless@126.com>
2021-09-14 20:52:15 +0800

    修复语法错误

--- site-lisp/js-mode/js-mode-conf.el
+++ site-lisp/js-mode/js-mode-conf.el
@@ -1,18 +1,16 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
 
 (defun js-mode-setting ()
   (interactive)
   (setq tab-width 2
         ;; this will make sure spaces are used instead of tabs
         indent-tabs-mode nil
-	setq forward-sexp-function nil
-		js-indent-level 2
-		js2-basic-offset 2))
+        forward-sexp-function nil
+        js-indent-level 2
+        js2-basic-offset 2))
 
 (add-hook 'js2-mode-hook 'js-mode-setting)
 
