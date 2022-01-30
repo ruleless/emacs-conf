23e9cd9b28e2fb86aa781596c04b64609b2c0c66
ruleless <ruleless@126.com>
2022-01-30 12:27:56 +0800

    modify settings for perl mode

--- site-lisp/misc-mode/perl-mode-conf.el
+++ site-lisp/misc-mode/perl-mode-conf.el
@@ -1,19 +1,22 @@
 ;; Use cperl-mode instead of the default perl-mode
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+
 (setq auto-mode-alist
       (append '(
                 ("\\.t\\'" . cperl-mode)
                 )
-			  auto-mode-alist))
-
+              auto-mode-alist))
 
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
-
 (defun my-cperl-setting ()
-  (setq indent-tabs-mode nil))
+  (setq tab-width 4
+        c-basic-offset 4
+        ;; this will make sure spaces are used instead of tabs
+        indent-tabs-mode nil)
+  (setq font-lock-maximum-decoration 1))
 
 (defun n-cperl-mode-hook ()
   (setq cperl-indent-level 4)
