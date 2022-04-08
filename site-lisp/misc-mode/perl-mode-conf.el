f5843e223c8b9aaf6b6e4b0896d83c77c4721be8
ruleless <ruleless@126.com>
2022-04-09 00:52:47 +0800

    optimize configration for perl-mode

--- site-lisp/misc-mode/perl-mode-conf.el
+++ site-lisp/misc-mode/perl-mode-conf.el
@@ -14,21 +14,18 @@
 (defun my-cperl-setting ()
   (setq tab-width 4
         c-basic-offset 4
+        cperl-indent-level 4
+        cperl-continued-statement-offset 0
+        cperl-extra-newline-before-brace t
+        cperl-indent-parens-as-block t
+        cperl-close-paren-offset -4
         ;; this will make sure spaces are used instead of tabs
         indent-tabs-mode nil)
-  (setq font-lock-maximum-decoration 1))
-
-(defun n-cperl-mode-hook ()
-  (setq cperl-indent-level 4)
-  (setq cperl-continued-statement-offset 0)
-  (setq cperl-extra-newline-before-brace t)
-  (setq cperl-indent-parens-as-block t)
   (set-face-background 'cperl-array-face "wheat")
   (set-face-background 'cperl-hash-face "wheat")
-  )
+  (setq font-lock-maximum-decoration 1))
 
 (add-hook 'cperl-mode-hook 'my-cperl-setting)
-(add-hook 'cperl-mode-hook 'n-cperl-mode-hook t)
 
 
 (provide 'perl-mode-conf)
